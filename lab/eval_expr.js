0a0d117678a80f91a1586d5c5fcfdcbab1e7e31c
phucknguyen <phucknguyen@gmail.com>
2016-12-14 09:51:28 +0700

    Keep making prototype of eval_expr

--- lab/eval_expr.js
+++ lab/eval_expr.js
@@ -38,10 +38,10 @@ function evalExp(str) {
     return ast;
 }
 
-// console.log(evalExp("1 + 2 * 3"));
-
-// // TODO need to handle operator precedence here
-// console.log(evalExp("1 * 2 + 3"));
+// Test evalExp
+if (false) {
+    console.log(evalExp("1 + 2 * 3"));
+}
 
 /**
  * Given an iterable of tokens of an expression,
@@ -57,7 +57,7 @@ function parseExp(tokens) {
         let text = tok.text;
         if (expect === "number") {
             if (type === "number") {
-                exp.left = text;
+                exp.head = text;
                 expect = "op";
             }
             else {
@@ -66,6 +66,7 @@ function parseExp(tokens) {
         }
         else if (expect === "op") {
             if (type === "operator") {
+                exp.left = exp.head;
                 exp.head = text;
                 exp.right = {};
                 exp = exp.right;
@@ -88,25 +89,44 @@ function parseExp(tokens) {
  * in pre-order and post-order visits, respectively.
  */
 function walk(btree, enter, leave) {
+    enter(btree);
     if (btree) {
-        enter(btree.head || btree);
-        walk(btree.left, enter, leave);
-        walk(btree.right, enter, leave);
-        leave(btree.head || btree);
+        if (btree.hasOwnProperty("left")) {
+            walk(btree.left, enter, leave);
+        }
+        if (btree.hasOwnProperty("right")) {
+            walk(btree.right, enter, leave);
+        }
     }
+    leave(btree);
 }
 
-/* Test
-walk({
-    head: "+",
-    left: {
-        head: "*",
-        left: 1,
-        right: 2 },
-    right: 3
-}, n => console.log("enter " + n),
-   n => console.log("leave " + n));
-*/
+// Test walk
+if (false) {
+    walk({
+        head: "+",
+        left: {
+            head: "*",
+            left: 1,
+            right: 2 },
+        right: 3
+    }, n => console.log("enter " + n),
+       n => console.log("leave " + n));
+}
+
+/**
+ * Walk binary tree, visit parents before children
+ */
+function prewalk(btree, visit) {
+    walk(btree, visit, n => {});
+}
+
+/**
+ * Walk binary tree, visit children before parents
+ */
+function postwalk(btree, visit) {
+    walk(btree, n => {}, visit);
+}
 
 /**
  * Return a map containing matching result from matching a btree with a pattern
@@ -118,25 +138,26 @@ function match(btree, pattern) {
     return isMatch ? matches : null;
 }
 
-/*
-const btree = {
-    head: "+", left: 1, right: { head: "*", left: 2, right: 3 } };
-const pattern = {
-    name: "sum",
-    head: "+", left: undefined, right: {} };
-console.log(match(btree, pattern));
+// Test match
+if (false) {
+    const btree = {
+        head: "+", left: 1, right: { head: "*", left: 2, right: 3 } };
+    const pattern = {
+        name: "sum",
+        head: "+", left: undefined, right: {} };
+    console.log(match(btree, pattern));
 
-const btree2 = {
-    head: "+", left: 1, right: { head: "*", left: 2, right: 3 } };
-const pattern2 = {
-    name: "sum",
-    head: "+", left: 1, right: { head: "-" } };
-console.log(match(btree2, pattern2));
-*/
+    const btree2 = {
+        head: "+", left: 1, right: { head: "*", left: 2, right: 3 } };
+    const pattern2 = {
+        name: "sum",
+        head: "+", left: 1, right: { head: "-" } };
+    console.log(match(btree2, pattern2));
+}
 
 /**
  * Internal use.
- * Some notes: for note value, undefined means whatever value,
+ * For note value, undefined means whatever value,
  * empty object means whatever object node.
  */
 function doMatch(btree, pattern, matches) {
@@ -168,31 +189,38 @@ function doMatch(btree, pattern, matches) {
 }
 
 /**
- * Fix operator precedence for btree that represents expressions.
+ * Pattern for btree that might need fixing operator precedence
+ */
+const precedencePattern = { name: "a",
+    head: undefined,
+    left: undefined,
+    right: { name: "b",
+        head: undefined,
+        left: undefined,
+        right: undefined
+    }
+}
+
+/**
+ * Get the fixed version for operator precedence for btree that represents
+ * expressions.
  *
  * If the tree looks like:
  * [* 1 [+ 2 3]]
  * then it should be changed to:
  * [+ [* 1 2] 3]
+ *
+ * pmap: map from operator to their precedence, higher value means higher
+ * precedence.
  */
-function getPrecedenceFix(btree, isHigher) {
-    const pattern = { name: "a",
-        head: undefined,
-        left: undefined,
-        right: { name: "b",
-            head: undefined,
-            left: undefined,
-            right: undefined
-        }
-    }
-
-    const matches = match(btree, pattern);
-    if (matches) {
-        let a = matches.get("a");
-        let b = matches.get("b");
-        if (isHigher(a.head, b.head)) {
-            // create a new tree
-            let fixed = {
+function getPrecedenceFix(btree, pmap) {
+    const m = match(btree, precedencePattern);
+    if (m) {
+        let a = m.get("a");
+        let b = m.get("b");
+        if (pmap.has(a.head) && pmap.has(b.head) && pmap.get(a.head) > pmap.get(b.head)) {
+            // make the fixed tree
+            return {
                 head: b.head,
                 left: {
                     head: a.head,
@@ -201,18 +229,149 @@ function getPrecedenceFix(btree, isHigher) {
                 },
                 right: b.right
             }
-            return fixed;
         }
     }
 }
 
-const requireFix = {
-    head: "*",
-    left: 1,
-    right: {
-        head: "+",
-        left: 2,
-        right: 3
+// Test getPrecedenceFix
+if (false) {
+    const tofix = {
+        head: "*",
+        left: 1,
+        right: {
+            head: "+",
+            left: 2,
+            right: 3
+        }
+    };
+
+    const pmap = new Map([["*", 2], ["+", 1]]);
+
+    console.log(getPrecedenceFix(tofix, pmap));
+}
+
+function fixOperatorPrecedence(btree) {
+    // for now simply consider * and +
+    const pmap = new Map([["*", 2], ["+", 1]]);
+
+    const fixPrecedence = function(n) {
+        if (n && n.left) {
+            let fixed = getPrecedenceFix(n.left, pmap);
+            if (fixed) {
+                n.left = fixed;
+            }
+        }
+        if (n && n.right) {
+            let fixed = getPrecedenceFix(n.right, pmap);
+            if (fixed) {
+                n.right = fixed;
+            }
+        }
     }
-};
-console.log(getPrecedenceFix(requireFix, (a, b) => a === "*" && b === "+"));
+
+    // fix the inner nodes
+    postwalk(btree, fixPrecedence);
+
+    // fix the root
+    let fixed = getPrecedenceFix(btree, pmap);
+    if (fixed) {
+        if (btree.hasOwnProperty("head")) {
+            btree.head = fixed.head;
+        }
+        if (btree.hasOwnProperty("left")) {
+            btree.left = fixed.left;
+        }
+        if (btree.hasOwnProperty("right")) {
+            btree.right = fixed.right;
+        }
+    }
+}
+
+// Test fixOperatorPrecedence
+if (false) {
+    const tofix = {
+        head: "*",
+        left: 1,
+        right: {
+            head: "+",
+            left: 2,
+            right: {
+                head: "*",
+                left: 3,
+                right: {
+                    head: "+",
+                    left: 4,
+                    right: 5
+                }
+            }
+        }
+    };
+
+    fixOperatorPrecedence(tofix);
+    console.log(tofix);
+}
+
+/**
+ * Make a new tree from an old one.
+ *
+ * f: map each old node to a new node, the new node should
+ * not have left and right children
+ */
+function map(btree, f) {
+    var top;
+
+    // stack of nodes visited in pre-order
+    const stack = [];
+
+    // entering, make new node on top of stack
+    const enter = n => {
+        // make new node
+        let newnode = f(n);
+        if (newnode.hasOwnProperty("left") || newnode.hasOwnProperty("right")) {
+            throw `Should not create left/right child for ${newnode}`;
+        }
+
+        // add new node to parent
+        if (stack.length > 0) {
+            let parent = stack[0];
+            if (parent.hasOwnProperty("left")) {
+                if (parent.hasOwnProperty("right")) {
+                    throw `Parent node ${parent} has both children already.`;
+                }
+                else {
+                    parent.right = newnode;
+                }
+            }
+            else {
+                parent.left = newnode;
+            }
+        }
+
+        stack.unshift(newnode);
+    };
+
+    const leave = n => {
+        // leaving, pop current node off of stack
+        top = stack.shift();
+    };
+
+    // create new nodes
+    walk(btree, enter, leave);
+
+    // last node visited in post-order is the (new) root
+    return top;
+}
+
+if (false) {
+    const tree = {
+        head: "*",
+        left: 1,
+        right: {
+            head: "+",
+            left: 2,
+            right: 3
+        }
+    };
+
+    console.log(map(tree, n => n.head ? { head: n.head } : n));
+}
